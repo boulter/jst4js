2919ea4908ef79c0653dc4fd68cfea0b06eab8e3
Jeff Boulter <boulter@bluefinlabs.com>
2012-04-26 17:34:15 -0400

    walk up template tree to find match for template

--- src/jst/ResourceTemplateLoader.java
+++ src/jst/ResourceTemplateLoader.java
@@ -1,34 +1,61 @@
 package jst;
 
+import java.io.File;
 import java.io.InputStream;
 import java.io.IOException;
 
 public class ResourceTemplateLoader implements TemplateLoader {
 
-    private String basePath;
+  private String basePath;
 
-    public ResourceTemplateLoader() {
-    }
+  public ResourceTemplateLoader() {
+  }
 
-    public ResourceTemplateLoader(String basePath) {
-        this.basePath = basePath;
-    }
+  public ResourceTemplateLoader(String basePath) {
+    this.basePath = basePath;
+  }
 
-    public InputStream load(String url) throws IOException {
+  private String buildResourcePath(String url) {
+    return "/" + (basePath != null ? basePath + "/" + url : url);
+  }
 
-      InputStream stream = getClass().getResourceAsStream( "/" + (basePath != null ? basePath + "/" + url : url) ); 
+  public InputStream load(String url) throws IOException {
+
+    File f = new File(url);
+
+    String filename = f.getName();
+
+    File parent = f;
+
+    // walk up the tree, looking for matches
+    
+    do {
+
+      parent = parent.getParentFile();
+
+      String path = (parent == null ? "" : parent.getPath() + File.separator ) + filename;
+
+      InputStream stream = getClass().getResourceAsStream(buildResourcePath(path));
 
       if (stream != null)
         return stream;
-      
-      return getClass().getResourceAsStream( "/" + (basePath != null ? basePath + "/" + url : url) + ".jst"); 
-    }
 
-    public boolean shouldRefresh(String url, long compiledTimestamp) {
-        return false;
-    }
+      stream = getClass().getResourceAsStream(buildResourcePath(path) + ".jst");
 
-    public String getRootUrl() {
-        return basePath;
-    }
+      if (stream != null)
+        return stream;
+
+    }  while (parent != null);
+
+
+    return null;
+  }
+
+  public boolean shouldRefresh(String url, long compiledTimestamp) {
+    return false;
+  }
+
+  public String getRootUrl() {
+    return basePath;
+  }
 }
